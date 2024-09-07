5b75e6741e9d81aed678506592a56b07e2bba15b
kousra <mdkousarmia71@gmail.com>
2024-09-07 13:51:10 +0600

    Math

---
+++ math.js
@@ -0,0 +1,20 @@
+// console.log(Math.max);
+// console.log(Math.min);
+
+const min = Math.min(45, 21, 65, 99, 126, 5, -199)
+console.log(min);
+
+const max = Math.max(45, 21, 65, 99, 126, 5, -199)
+console.log(max);
+console.log(Math.PI);
+console.log(Math.abs(500-20));
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+console.log(Math.round(2.55));
+console.log(Math.floor(2.55));
+console.log(Math.ceil(2.55));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+const rend = Math.round(Math.random()*10)
+console.log(rend);
