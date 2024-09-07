8ce3f4320e805a0a5a281ac9391c369f3138609c
kousra <mdkousarmia71@gmail.com>
2024-09-07 14:09:29 +0600

    date

---
+++ date.js
@@ -0,0 +1,8 @@
+const today =  new Date(2024-09-7);
+console.log(today.getMonth());
+console.log(today.getDay());
+
+const specificDate = new Date(2091, 0, 26)
+console.log(specificDate);
+specificDate.setMonth(10);
+console.log(specificDate);
